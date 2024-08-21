1508bf22fb355d218367fa4c2b1859feb902c161
Kennedy M <menezes_kennedy@hotmail.com>
2024-08-20 23:23:26 -0300

    step-2-adding-three-numbers

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,10 @@ def test_add_two_numbers():
     result = calculator.add(4,5)
 
     assert result == 9
+
+def test_add_three_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4,5,6)
+
+    assert result == 15
